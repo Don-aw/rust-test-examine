e9feb62bfe3f161e349491f4d3de381411d3ff56
donaw <177387755+Don-aw@users.noreply.github.com>
2024-11-20 08:19:55 +0800

    Add parse file structure method to Parser

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,14 @@
+import java.io.File;
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
         Parser p = new Parser();
-        p.file_structure();
+
+        //file path of rust-test-examine tool
+        File root = new File("");
+
+        ArrayList<File> suites = p.file_structure(root);
         p.given_env();
     }
 }
--- src/Parser.java
+++ src/Parser.java
@@ -1,19 +1,24 @@
 import java.io.File;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Map;
 
 public class Parser {
 
-    //file path of rust-test-examine tool
-    File root = new File("");
-
-    public void file_structure() {
+    public ArrayList<File> file_structure(File root) {
+        ArrayList<File> files = new ArrayList<>();
         File folder = new File(Paths.get(root.getAbsolutePath()).getParent().toString().concat("/rust/tests"));
 
         for (File file : folder.listFiles()) {
-            System.out.println(file.getName());
+            String filename = file.getName();
+            if (filename.contains(".") || filename.contains("rustdoc")) continue;
+            files.add(file);
         }
+
+        System.out.println(files.toString());
+
+        return files;
 
     }
 
