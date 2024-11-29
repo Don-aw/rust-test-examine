9c751bdd4066534b82e77441df09633d1135e9e9
donaw <177387755+Don-aw@users.noreply.github.com>
2024-11-29 15:59:42 +0800

    Change data structure to map in parse filter

--- src/Parser.java
+++ src/Parser.java
@@ -1,9 +1,6 @@
 import java.io.*;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Map;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Parser {
 
@@ -24,94 +21,69 @@ public class Parser {
 
     }
 
-    public void parse_filter(File f) throws IOException {
+    public HashMap<String, ArrayList<ArrayList<String>>> parse_filter(File f) throws IOException {
 
         BufferedReader r = new BufferedReader(new FileReader(f));
-        ArrayList<String> directives_all = new ArrayList<>();
-        ArrayList<ArrayList<String>> directives_rev = new ArrayList<>();
+
+        HashMap<String, ArrayList<ArrayList<String>>> filters = new HashMap<>();
+        addRevision("main", filters);
+
+        /*
+            {
+                "*revision name*":
+                    [*only*],
+                    [*ignore*],
+                    [*needs*],
+                    [*others*]
+            }
+         */
+
         String line = r.readLine();
-        ArrayList<String> revisions = new ArrayList<>();
-
         while (line != null) {
-            if (line.contains("//@ revisions:")) {
-                //splice the "//@ revisions: " out
-                StringTokenizer st = new StringTokenizer(line.substring(14));
+            if (line.contains("//@ ")) {
 
-                while (st.hasMoreTokens()) {
-                    revisions.add(st.nextToken());
-                    directives_rev.add(new ArrayList<>());
-                }
+                line = line.substring(4);
+                //splice the "//@ " out
 
-            }
-            else if (!revisions.isEmpty()) {
-                for (int i = 0; i < revisions.size(); i++) {
-                    if (line.contains("//@ [" + revisions.get(i))) {
-                        directives_rev.get(i).add(line.substring(7 + revisions.get(i).length()));
+                if (line.contains("revisions:")) {
+                    //splice the "revisions: " out
+                    StringTokenizer st = new StringTokenizer(line.substring(11));
+
+                    while (st.hasMoreTokens()) {
+                        addRevision(st.nextToken(), filters);
                     }
+
+                } else if (line.contains("[")) {
+
+                    for (String i : filters.keySet()) {
+                        if (line.contains(i)) {
+
+                            line = line.substring(3 + i.length());
+
+                            if (line.contains("only")) filters.get(i).get(0).add(line.substring(5));
+                            else if (line.contains("ignore")) filters.get(i).get(1).add(line.substring(7));
+                            else if (line.contains("needs")) filters.get(i).get(2).add(line.substring(6));
+                            else filters.get(i).get(3).add(line);
+
+                        }
+                    }
+
+
+                } else {
+
+                    if (line.contains("only")) filters.get("main").get(0).add(line.substring(5));
+                    else if (line.contains("ignore")) filters.get("main").get(1).add(line.substring(7));
+                    else if (line.contains("needs")) filters.get("main").get(2).add(line.substring(6));
+                    else filters.get("main").get(3).add(line);
                 }
             }
-            else if (line.contains("//@ ")) directives_all.add(line.substring(4));
 
             line = r.readLine();
         }
 
-        System.out.println("revisions: " + revisions);
-        //System.out.println(directives_rev);
-        //System.out.println(directives_all);
+        System.out.println(filters);
 
-        ArrayList<String> only_all = new ArrayList<>();
-        ArrayList<String> ignore_all = new ArrayList<>();
-        ArrayList<String> needs_all = new ArrayList<>();
-        ArrayList<String> other_all = new ArrayList<>();
-
-        while (!directives_all.isEmpty()) {
-            String d = directives_all.getFirst();
-            if (d.contains("only")) only_all.add(d.substring(5));
-            else if (d.contains("ignore")) ignore_all.add(d.substring(7));
-            else if (d.contains("needs")) needs_all.add(d.substring(6));
-            else other_all.add(d);
-
-            directives_all.removeFirst();
-        }
-
-        System.out.println("(all) only:   " + only_all);
-        System.out.println("(all) ignore: " + ignore_all);
-        System.out.println("(all) needs:  " + needs_all);
-        System.out.println("(all) other:  " + other_all);
-
-        ArrayList<ArrayList<String>> only_rev = new ArrayList<>();
-        ArrayList<ArrayList<String>> ignore_rev = new ArrayList<>();
-        ArrayList<ArrayList<String>> needs_rev = new ArrayList<>();
-        ArrayList<ArrayList<String>> other_rev = new ArrayList<>();
-
-        for (ArrayList<String> dr : directives_rev) {
-            only_rev.add(new ArrayList<>());
-            ignore_rev.add(new ArrayList<>());
-            needs_rev.add(new ArrayList<>());
-            other_rev.add(new ArrayList<>());
-
-            while (!dr.isEmpty()) {
-                String d = dr.getFirst();
-                if (d.contains("only")) only_rev.getLast().add(d.substring(5));
-                else if (d.contains("ignore")) ignore_rev.getLast().add(d.substring(7));
-                else if (d.contains("needs")) needs_rev.getLast().add(d.substring(6));
-                else other_all.add(d);
-
-                dr.removeFirst();
-            }
-
-        }
-
-
-        for (int i = 0; i < directives_rev.size(); i++) {
-            System.out.println("(" + revisions.get(i) + ") only:   " + only_rev.get(i));
-            System.out.println("(" + revisions.get(i) + ") ignore:   " + ignore_rev.get(i));
-            System.out.println("(" + revisions.get(i) + ") needs:   " +  needs_rev.get(i));
-            System.out.println("(" + revisions.get(i) + ") only:   " + other_rev.get(i));
-        }
-
-
-
+        return filters;
 
     }
 
@@ -126,4 +98,11 @@ public class Parser {
 
     }
 
+    public void addRevision(String revName, HashMap h) {
+        ArrayList<ArrayList<String>> temp = new ArrayList<>();
+        for (int i = 0; i < 4; i++) {
+            temp.add(new ArrayList<>());
+        }
+        h.put(revName, temp);
+    }
 }
